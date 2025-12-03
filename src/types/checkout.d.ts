364d196957a2c6477a620bc2d6f94792f0f7f442
khoa-anhtran <khoa.anhtran@infodation.vn>
2025-12-03 10:08:40 +0700

    add PaymentStatus state

---
+++ src/types/checkout.d.ts
@@ -0,0 +1,36 @@
+import { PayloadAction } from "."
+import { shippingAddressSchema } from "../pages/checkout/components/ShippingAddress"
+
+export type Commune = {
+    code: string,
+    name: string,
+    englishName: string,
+    administrativeLevel: string,
+    provinceCode: string,
+    provinceName: string,
+    decree: string
+}
+
+export type Province = {
+    code: string,
+    name: string,
+    englishName: string,
+    administrativeLevel: string,
+    decree: string
+}
+
+export type CheckoutState = {
+    provinces: Province[],
+    communes: Commune[],
+    shippingAddress: ShippingAddressType,
+    paymentStatus: {
+        type: string,
+        isPaid: boolean
+    }
+    status: string,
+    error: string | null
+}
+
+export type CheckoutPayloadAction = PayloadAction<>
+
+export type ShippingAddressType = z.infer<typeof shippingAddressSchema>;

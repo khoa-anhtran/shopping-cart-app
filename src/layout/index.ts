e2457de40d54074192607f443efd8f586775dade
khoa-anhtran <khoa.anhtran@infodation.vn>
2025-12-09 15:02:19 +0700

    add index file for layout

---
+++ src/layout/index.ts
@@ -0,0 +1,6 @@
+import CheckoutLayout from "./CheckoutLayout"
+import Header from "./Header"
+import MainLayout from "./MainLayout"
+import ProductLayout from "./ProductLayout"
+
+export { CheckoutLayout, Header, MainLayout, ProductLayout }
